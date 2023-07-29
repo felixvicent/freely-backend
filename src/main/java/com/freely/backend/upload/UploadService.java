0e7e32fdb96244c847be1414ed44d3f7014b5820
Félix Vicente <felixvicent1306@gmail.com>
2023-07-29 17:22:37 -0300

    add bad request exception

--- src/main/java/com/freely/backend/upload/UploadService.java
+++ src/main/java/com/freely/backend/upload/UploadService.java
@@ -9,6 +9,8 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.freely.backend.exceptions.BadRequestException;
+
 @Service
 public class UploadService {
   public static final String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
@@ -16,6 +18,11 @@ public class UploadService {
   public String uploadImage(MultipartFile file, String folder) throws IOException {
 
     String filename = file.getOriginalFilename();
+
+    if (filename == null) {
+      throw new BadRequestException("Arquivo não enviado");
+    }
+
     String fileExtension = filename.substring(filename.lastIndexOf(".") + 1);
     StringBuilder fileNames = new StringBuilder();
     fileNames.append(UUID.randomUUID().toString()).append(".").append(fileExtension);
