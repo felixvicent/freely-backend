6389e377eb52a55ee66fa73c9a7a434c10cf2b76
Félix Vicente <felixvicent1306@gmail.com>
2024-03-30 17:28:07 -0300

    add healthcheck endpoint to permitable

--- src/main/java/com/freely/backend/authentication/SecurityConfiguration.java
+++ src/main/java/com/freely/backend/authentication/SecurityConfiguration.java
@@ -34,6 +34,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf()
                 .disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
